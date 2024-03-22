dcca1884b2aed33b5f3a6a3bf603a4a6584dba3e
NFPerera <niko22602@gmail.com>
2024-03-22 19:05:44 -0300

    Ui added to fine tunning variables in game
    - added a little Translucid arena for the boids to fly inside

---
+++ Assets/_Main/Scripts/Managers/UiManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Main.Scripts.Managers
+{
+    public class UiManager : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> allUiElements = new List<GameObject>();
+
+        [Header("Steerings Stats")]
+        [SerializeField] private TMP_Text obsAvoidTxt;
+        [SerializeField] private TMP_Text cohesionTxt;
+        [SerializeField] private TMP_Text alignmentTxt;
+        [SerializeField] private TMP_Text alignmentRadiusTxt;
+        [SerializeField] private TMP_Text cohesionRadiusTxt;
+        
+        [Header("Boids Stats")]
+        [SerializeField] private TMP_Text speedTxt;
+        [SerializeField] private TMP_Text viewRangeTxt;
+        [SerializeField] private TMP_Text viewAngleTxt;
+
+        public void SetBoidSpeed(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidsSpeed(p_f);
+            speedTxt.text = p_f.ToString();
+        }
+        
+        public void SetBoidViewRange(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidViewRange(p_f);
+            viewRangeTxt.text = p_f.ToString();
+        }
+        
+        public void SetBoidViewAngle(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidViewAngle(p_f);
+            viewAngleTxt.text = p_f.ToString();
+        }
+        
+        public void SetBoidObsAvoid(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidObsAvoid(p_f);
+            obsAvoidTxt.text = p_f.ToString();
+        }
+        public void SetBoidCohesion(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidCohesion(p_f);
+            cohesionTxt.text = p_f.ToString();
+        }
+        public void SetBoidAlignment(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidAlignment(p_f);
+            alignmentTxt.text = p_f.ToString();
+        }
+        public void SetBoidAlignmentRadius(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidAlignmentRadius(p_f);
+            alignmentRadiusTxt.text = p_f.ToString();
+        }
+        public void SetBoidCohesionRadius(float p_f)
+        {
+            BoidsManager.Singleton.SetBoidCohesionRadius(p_f);
+            cohesionRadiusTxt.text = p_f.ToString();
+        }
+        
+        
+        public void ToggleUi()
+        {
+            foreach (var obj in allUiElements)
+            {
+                obj.SetActive(!obj.activeSelf);
+            }
+        }
+        
+        
+    }
+}
