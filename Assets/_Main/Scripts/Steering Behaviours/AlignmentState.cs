878ff0e88d00a1e35ae8215b0b0518d1e8314a80
NFPerera <niko22602@gmail.com>
2024-03-21 21:44:53 -0300

    Cleaning of not usefull scripts

---
+++ Assets/_Main/Scripts/Steering Behaviours/AlignmentState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Main.Scripts.Steering_Behaviours
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class AlignmentState : ScriptableObject
+    {
+        
+    }
+}
