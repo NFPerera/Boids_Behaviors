0304726c7ef42a4b6404b221c082d029ba3b162d
NFPerera <niko22602@gmail.com>
2024-06-19 22:22:25 -0300

    Fix in teleportation

--- Assets/_Main/Scripts/WallCollisionTeleporter.cs
+++ Assets/_Main/Scripts/WallCollisionTeleporter.cs
@@ -6,15 +6,19 @@ namespace _Main.Scripts
 {
     public class WallCollisionTeleporter : MonoBehaviour
     {
-        
-
         private void OnCollisionEnter(Collision p_other)
         {
             if (!p_other.gameObject.TryGetComponent(out BoidsModel l_boid))
                 return;
 
-            var l_prevPos = l_boid.transform.position;
-            l_boid.transform.position = -l_prevPos;
+            Vector3 l_contactPoint = p_other.GetContact(0).point;
+
+            //Esto esta mal, pero creo que funcione
+            var l_newPos = -l_contactPoint - (-l_contactPoint).normalized;
+            // Teleport the entity to the new position
+            l_boid.transform.position = l_newPos;
+            
+            
         }
     }
 }
